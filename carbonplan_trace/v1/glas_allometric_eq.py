dcb711f6208b14799b882d3e2c49469bf496fb96
tcchiao <tcchiao@gmail.com>
2021-02-16 18:30:47 +0000

    fix

--- carbonplan_trace/v1/glas_allometric_eq.py
+++ carbonplan_trace/v1/glas_allometric_eq.py
@@ -1,14 +1,16 @@
-
-
 def tsui_etal_2012(ds):
-    return -7.144 - 12.925 * ds['meanH_height'] + 2.239 * ds['10th_height'] + 14.019 * ds['90th_height']
+    return (
+        -7.144
+        - 12.925 * ds["meanH_height"]
+        + 2.239 * ds["10th_height"]
+        + 14.019 * ds["90th_height"]
+    )
+
 
 def apply_allometric_equation(ds):
-    ds['biomass'] = tsui_etal_2012(ds)
-    
+    ds["biomass"] = tsui_etal_2012(ds)
+
     return ds
 
 
-ALLOMETRIC_EQUATIONS_MAP = {
-    
-}
+ALLOMETRIC_EQUATIONS_MAP = {}
