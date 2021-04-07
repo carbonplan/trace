de9d42a0d139001c646eb83beb73f2e3682925c0
Joseph Hamman <jhamman@ucar.edu>
2021-04-07 13:56:41 +0000

    add srtm download script and update glas zarr conversion script

--- scripts/preprocess_glas.py
+++ scripts/preprocess_glas.py
@@ -7,7 +7,6 @@ import xarray as xr
 import zarr
 from dask.distributed import Client
 from gcsfs import GCSFileSystem
-from tqdm import tqdm
 
 from carbonplan_trace.v1.glas_extract import extract_GLAH01_data, extract_GLAH14_data
 
@@ -68,8 +67,8 @@ process_funcs = {'GLAH01': process_01, 'GLAH14': process_14}
 
 
 def granules_h5_to_zarr(products):
-    # List all files in carbonplan-scratch
-    files = fs.ls('carbonplan-scratch/glas-cache')
+    # List all files in raw folder
+    files = fs.ls('carbonplan-climatetrace/inputs/glas-raw/')
     files.sort()
 
     tasks = defaultdict(list)
@@ -105,15 +104,17 @@ def lazy_open(uri):
 def main(products=['GLAH01', 'GLAH14']):
 
     # convert hdf5 granules to zarr
+    print('granules_h5_to_zarr')
     results = granules_h5_to_zarr(products)
 
     for p, presults in results.items():
         uris = presults['skipped'] + presults['converted']
 
+        print('open xarray datasets')
         ds_list = dask.compute([lazy_open(uri) for uri in uris])[0]
 
         print('concat list of datasets')
-        with dask.config.set(**{'array.slicing.split_large_chunks': False}):
+        with dask.config.set(**{'array.slicing.split_large_chunks': True}):
             ds = xr.concat(ds_list, dim='record_index').chunk({'record_index': chunksize})
             for k in ds:
                 _ = ds[k].encoding.pop('chunks', None)
@@ -121,16 +122,20 @@ def main(products=['GLAH01', 'GLAH14']):
         # write
         print(f'writing {p}')
         print(ds)
-        mapper = fsspec.get_mapper(f'gs://carbonplan-scratch/{p.lower()}.zarr')
+        print(f'ds.nbytes: {ds.nbytes / 1e9}')
+        mapper = fsspec.get_mapper(f'gs://carbonplan-climatetrace/intermediates/{p.lower()}.zarr')
         mapper.clear()
 
-        _ = ds.to_zarr(mapper, compute=False, mode='w')
-        stepsize = chunksize * 100
+        # print('writing zarr dataset')
+        ds.to_zarr(mapper, compute=False, mode='w')
+        stepsize = chunksize * 1
         recs = ds.dims['record_index']
-        for left, right in tqdm(
-            zip(range(0, recs, stepsize), range(stepsize, recs + stepsize, stepsize))
+        print('writing zarr dataset chunks')
+        for left, right in zip(
+            range(0, recs, stepsize), range(stepsize, recs + stepsize, stepsize)
         ):
             s = slice(left, right)
+            print(s, flush=True)
             ds.isel(record_index=s).drop(drop_keys[p]).to_zarr(mapper, region={'record_index': s})
 
         zarr.consolidate_metadata(mapper)
@@ -142,4 +147,4 @@ if __name__ == '__main__':
     print(client)
     print(client.dashboard_link)
 
-    main(products=['GLAH14'])
+    main(products=['GLAH01'])
