add to environment file and fix nan-handling in seasonal averaging

carbonplan_trace/v1/landsat_preprocess.py
@@ -100,23 +100,19 @@ def grab_ds(item, bands_of_interest, cog_mask, utm_zone, utm_letter):
     ds = xr.concat(da_list, dim='band').to_dataset(dim='band').rename({1: 'reflectance'})
     del da_list
     ds = ds.assign_coords({'band': bands_of_interest})
-    print(ds.nbytes)
     ds = ds.where(ds != 0)
-    print('in the where')
-    print(ds.nbytes)
     ds = ds.where(cog_mask < 2)
-    print(ds.nbytes)
     ds = ds.reflectance.to_dataset(dim='band')
     ds.attrs["utm_zone_number"] = utm_zone
     ds.attrs["utm_zone_letter"] = utm_letter
-    print(ds.nbytes)
-    print('add ndvi ndii')
     ds = calc_NDVI(ds)
     ds = calc_NDII(ds)
     gc.collect()
-    print(ds.nbytes)
-    print('finish up grabbing')
     return ds
+
+
+def valid_pixel_mask(ds):
+    return (~ds['SR_B1'].isnull()).astype(int).compute()
 
 
 def average_stack_of_scenes(ds_list):
@@ -142,43 +138,24 @@ def average_stack_of_scenes(ds_list):
     for ds in ds_list:
         utm_zone.append(ds.attrs['utm_zone_number'])
         utm_letter.append(ds.attrs['utm_zone_letter'])
-        print('dataset is size')
-        print(ds.nbytes)
     # WATCH OUT: you don't want to average scenes from multiple utm projections!!
     # thank goodness we have these two swanky assertions below
     # TODO: this could probably be moved to a test
     assert len(set(utm_zone)) == 1
     assert len(set(utm_letter)) == 1
-    print('averaging now!!')
     # less memory-intensive way of averaging
-    number_of_datasets = len(ds_list)
-    full_ds = ds_list.pop()
+    full_ds = ds_list.pop().load()
+    valid_pixel_count = valid_pixel_mask(full_ds)
     while ds_list:
-        full_ds = sum([full_ds, ds_list.pop()]).compute()
-
-    full_ds = full_ds / len(ds_list)
-    # full_ds = (sum(ds_list)/len(ds_list))#.compute()
-    # full_ds = xr.concat(ds_list, dim='scene')#.astype('int16') #, coords='minimal'
-    # print('this is after concat')
-    # print(full_ds.nbytes)
-    # # full_ds = full_ds.mean(dim='scene')#.astype('int16')
-    # print('this is after mean')
-    # print(full_ds.nbytes)
-    # gc.collect()
-    # full_ds = full_ds.compute()
-    # gc.collect()
-    # print(full_ds)
-    # print(full_ds.nbytes)
-    # # print(full_ds.shape)
-    # # averaged = full_ds.mean(axis=0)
-    # # print(averaged.shape)
-    # # print(full_ds.nbytes)
-
-    # # for var in ['SR_B1', 'SR_B2', 'SR_B3', 'SR_B4', 'SR_B5', 'SR_B7']:
-    # #     full_ds[var] = full_ds[var].astype('int16')
-    # #     print(full_ds.nbytes)
-    # print(full_ds.nbytes)
-    # del ds_list
+        ds = ds_list.pop().load()
+        full_ds += ds.compute()
+        mask = valid_pixel_mask(ds).load()
+        valid_pixel_count = (valid_pixel_count + mask).load()
+        del mask
+        del ds
+    # divide by the number of active pixels to get your seasonal average
+    full_ds = full_ds / valid_pixel_count
+    del valid_pixel_count
 
     full_ds.attrs['utm_zone_number'] = utm_zone[0]
     full_ds.attrs['utm_zone_letter'] = utm_letter[0]
@@ -311,11 +288,7 @@ def calc_NDVI(ds):
 
     nir = ds['SR_B4']
     red = ds['SR_B3']
-    print('calculating ndvi')
     ds['NDVI'] = (nir - red) / (nir + red)
-    print(ds['NDVI'].nbytes)
-    # ds['NDVI'] = (ds['NDVI']*1000)#.astype('int16')
-    print(ds['NDVI'].nbytes)
     return ds
 
 
@@ -387,23 +360,12 @@ def scene_seasonal_average(
             # ds_list = None
             for file in valid_files:
                 scene_id = file[-40:]
-                print(scene_id)
                 url = 's3://{}/{}'.format(file, scene_id)
                 utm_zone, utm_letter = get_scene_utm_info(url)
                 cloud_mask_url = url + '_SR_CLOUD_QA.TIF'
                 cog_mask = cloud_qa(cloud_mask_url)
                 ds_list.append(grab_ds(url, bands_of_interest, cog_mask, utm_zone, utm_letter))
-                # if ds_list is not None:
-                #     ds_list = np.concatenate((ds_list, grab_ds(url, bands_of_interest, cog_mask, utm_zone, utm_letter).values), dtype='int16')
-                # else:
-                #     ds_list = grab_ds(url, bands_of_interest, cog_mask, utm_zone, utm_letter).values
-                # print(ds_list[-1].nbytes)
-                # print(ds_list.size * ds_list.itemsize)
-                # ds_list = np.array(ds_list)
-            print('averaging now')
             seasonal_average = average_stack_of_scenes(ds_list)
-            print(seasonal_average.nbytes)
-            print(seasonal_average)
             del ds_list
             if write_bucket is not None:
                 # set where you'll save the final seasonal average