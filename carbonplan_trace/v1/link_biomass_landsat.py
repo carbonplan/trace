a1efa95e3ac00a9be4bf260ef216b6138e68acb9
Oriana <orianac@uw.edu>
2021-06-17 05:59:09 +0000

    training prep utilities

--- carbonplan_trace/v1/link_biomass_landsat.py
+++ carbonplan_trace/v1/link_biomass_landsat.py
@@ -1,169 +1,128 @@
+import gc
+import os
 from datetime import datetime
 
+import boto3
+import dask
+import fsspec
+import numpy as np
 import pandas as pd
+import rasterio as rio
 import rioxarray  # for the extension to load
 import utm
 import xarray as xr
+from pyproj import CRS
+from rasterio.session import AWSSession
 from s3fs import S3FileSystem
+
+from carbonplan_trace.v1 import load, utils
+from carbonplan_trace.v1.inference import reproject_dataset_to_fourthousandth_grid
+from carbonplan_trace.v1.landsat_preprocess import scene_seasonal_average
 
 # flake8: noqa
 
 fs = S3FileSystem(profile='default', requester_pays=True)
 
 
-def add_landsat_utm_zone(scene_gdf):
-    '''
-    Grab sample file for each landsat scene and
-    extract the utm zone then add that to the
-    scene geodataframe
-
-    Parameters
-    ----------
-    scene_gdf : geopandas geodataframe
-        geodataframe whose rows are entries for each row/path scene
-
-    Returns
-    -------
-    scene_gdf : geopandas geodataframe
-        same geodataframe but with new column including the USGS-provided,
-        scene-specific UTM zone
-
-    '''
-    scene_gdf['landsat_utm_zone'] = ""
-    for scene_id in scene_gdf.index:
-        scene_row = scene_gdf.loc[scene_id]['ROW']
-        scene_path = scene_gdf.loc[scene_id]['PATH']
-        url = 's3://carbonplan-climatetrace/v1/{}/{}/2004/JJA_reflectance.zarr'.format(
-            scene_path, scene_row
-        )
-        landsat_utm_zone = xr.open_zarr(fs.get_mapper(url)).utm_zone
-        scene_gdf.loc[scene_id, 'landsat_utm_zone'] = int(landsat_utm_zone)
-    return scene_gdf
+def add_aster_worldclim(data, tiles, lat_lon_box=None):
+    data = load.aster(data, tiles, lat_lon_box=lat_lon_box)
+    data = load.worldclim(data)
+    return data
 
 
-def convert_to_utm(df):
-    '''
-    Given dataframe with lat/lon coordinates of GLAS shots, project
-    into the correct x/y coordinates to link to the Landsat scene.
-
-    Parameters
-    ----------
-    df : pandas dataframe
-        geodataframe whose rows are entries for each row/path scene. Must
-        include variables 'lon', 'lat', and a utm zone called 'landsat_utm_zone'
-
-    Returns
-    -------
-    df : pandas dataframe
-        The projected information for each shot
-    '''
-
-    return utm.from_latlon(df['lat'], df['lon'], force_zone_number=df['landsat_utm_zone'])
-
-
-def grab_year(df):
-    '''
-    Access calendar year in timestamp within a dataframe
-    '''
-    return datetime.fromtimestamp(df['time']).year
-
-
-def add_projection_info(df):
-    '''
-    Given dataframe with lat/lon coordinates of GLAS shots,
-    add UTM zone (as specified by USGS landsat product) and use
-    that zone to translate GLAS shot location into projected x/y space
-
-    Parameters
-    ----------
-    df : pandas dataframe
-        geodataframe whose rows are entries for each row/path scene. Must
-        include variables 'lon', 'lat', and a utm zone called 'landsat_utm_zone'
-
-    Returns
-    -------
-    updated_df : pandas dataframe
-        The df with the projected location of each GLAS shot
-    '''
-
-    projection_info = df.apply(convert_to_utm, axis=1).to_list()
-    projected_column_names = ['proj_x', 'proj_y', 'utm_zone', 'utm_letter']
-    projection_df = pd.DataFrame(projection_info, columns=projected_column_names, index=df.index)
-    updated_df = pd.concat([df, projection_df], axis=1)
-    return updated_df
-
-
-def build_url(df):
-    '''
-    Add landsat url to dataframe
-    Parameters
-    ----------
-    df : pandas dataframe
-        geodataframe whose rows are entries for each row/path scene. Must
-        include variables 'PATH', 'ROW', and 'year'
-
-    Returns
-    -------
-    df : pandas dataframe
-        The df with a the url for the landsat scene encompassing each shot
-    '''
-    return 's3://carbonplan-climatetrace/v1/{}/{}/{}/JJA_reflectance.zarr'.format(
-        df['PATH'], df['ROW'], df['year']
-    )
-
-
-def add_linking_info(df):
-    '''
-    Given dataframe with lat/lon coordinates of GLAS shots,
-    add all necessary linking info- projection, year, and url
-    (which will act as the way to add landsat)
-    Parameters
-    ----------
-    df : pandas dataframe
-        geodataframe whose rows are entries for each row/path scene. Must
-        include variables 'lon', 'lat', and a utm zone called 'landsat_utm_zone'
-
-    Returns
-    -------
-    df : pandas dataframe
-        The df with the projected location of each GLAS shot
-    '''
-    df = add_projection_info(df)
-    df['year'] = df.apply(grab_year, axis=1)
-    df['url'] = df.apply(build_url, axis=1)
-    return df
-
-
-def create_combined_landsat_biomass_df(
-    landsat_ds, biomass_df, biomass_variables=['biomass', 'glas_elev', 'ecoregion']
+def prep_training_dataset(
+    path,
+    row,
+    year,
+    access_key_id,
+    secret_access_key,
+    training_write_bucket=None,
+    bands_of_interest='all',
+    season='JJA',
 ):
+    core_session = boto3.Session(
+        aws_access_key_id=access_key_id,
+        aws_secret_access_key=secret_access_key,
+        region_name='us-west-2',
+    )
+    aws_session = AWSSession(core_session, requester_pays=True)
+    fs = S3FileSystem(key=access_key_id, secret=secret_access_key, requester_pays=True)
+    with dask.config.set(
+        scheduler='single-threaded'
+    ):  # this? **** #threads #single-threaded # threads??
+        with rio.Env(aws_session):
+
+            # create the landsat scene for that year
+            landsat_ds = scene_seasonal_average(
+                path,
+                row,
+                year,
+                access_key_id,
+                secret_access_key,
+                aws_session,
+                core_session,
+                fs,
+                write_bucket='s3://carbonplan-climatetrace/v1/',
+                bands_of_interest='all',
+                season=season,
+                landsat_generation='landsat-7',
+            )
+            # add in other datasets
+            landsat_zone = landsat_ds.utm_zone_number + landsat_ds.utm_zone_letter
+            data, tiles, bounding_box = reproject_dataset_to_fourthousandth_grid(
+                landsat_ds, zone=landsat_zone
+            )
+            del landsat_ds
+            data = add_aster_worldclim(data, tiles, lat_lon_box=bounding_box).load()
+            # here we take it to dataframe
+            df = create_combined_landsat_biomass_df(data, tiles, year)
+            del data
+            utils.write_parquet(df, training_write_bucket, access_key_id, secret_access_key)
+
+
+def create_combined_landsat_biomass_df(data, tiles, year):
     '''
     Add landsat info for each biomass entry
 
     Parameters
     ----------
-    landsat_ds : xarray dataset
+    data : xarray dataset
         landsat dataset to extract information from
     biomass_df : pandas dataframe
-        dataframe with biomass and all information for linking to
+        dataframe with biomass, ancillary variables, and lat lon for linking to
         landsat
-    biomass_variables : list
-        List of variables to retain from original biomass_df
+    year : pandas dataframe
+        the year from the landsat scene that you want to grab from biomass_df
     Returns
     -------
-    out_df : pandas dataframe
+    df : pandas dataframe
         The df with the biomass (and potentially other variables) and
         the corresponding landsat data
     '''
-    selected_landsat = (
-        landsat_ds.sel(
-            x=xr.DataArray(biomass_df['proj_x'].values, dims='shot'),
-            y=xr.DataArray(biomass_df['proj_y'].values, dims='shot'),
+    biomass_variables = (
+        [f'BIO{str(n).zfill(2)}' for n in range(1, 20)]
+        + ['burned', 'treecover2000_mean']
+        + ['prec', 'srad', 'tavg', 'tmax', 'tmin', 'vapr', 'wind']
+        + ['elev', 'slope', 'aspect', 'lat', 'lon']
+    )
+    # open all the biomass tiles
+    # don't need to do the bounding box trimming because
+    # it isn't spatial data (it's df)
+    biomass_df = load.biomass(tiles, year)[biomass_variables]
+    # TODO CHECK THIS LINE!!
+    df = (
+        data.sel(
+            x=xr.DataArray(biomass_df['lon'].values, dims='shot'),
+            y=xr.DataArray(biomass_df['lat'].values, dims='shot'),
             method='nearest',
         )
         .to_dataframe()
-        .drop(['x', 'y'], axis=1)
+        .drop(['spatial_ref'], axis=1)
     )
-    selected_landsat.index = biomass_df.index
-    out_df = pd.concat([biomass_df[biomass_variables], selected_landsat], axis=1)
-    return out_df
+    del data
+    df.index = biomass_df.index
+    df = pd.concat([biomass_df, df], axis=1)
+
+    # remove any values that got masked - we might want to do some tolerance here
+    df = df.dropna(how='any')
+    return df
