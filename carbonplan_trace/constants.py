37ae79eb069de509616de97732438382ed560c72
Cindy Chiao <tcchiao@gmail.com>
2021-08-31 02:37:27 +0000

    update

--- carbonplan_trace/constants.py
+++ carbonplan_trace/constants.py
@@ -2,8 +2,3 @@ TC02_PER_TC = 3.67
 TC_PER_TBM = 0.5
 SQM_PER_HA = 10000
 R = 6.371e6
-EMISSIONS_FACTORS = {
-    'savanna_fires': 1.613,
-    'tropical_forest_fires': 1.580,
-    'extratropical_forest_fires': 1.569,
-}
