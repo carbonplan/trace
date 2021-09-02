e339dcf74e7dc4dd33ab5293f79bbd2f3f9c9edf
Cindy Chiao <tcchiao@gmail.com>
2021-09-02 01:48:18 +0000

    changes

--- carbonplan_trace/v1/change_point_detection.py
+++ carbonplan_trace/v1/change_point_detection.py
@@ -1,9 +1,12 @@
+from datetime import datetime
+
+import numpy as np
 import scipy
 import xarray as xr
 
 
 def calc_rss(ytrue, ypred):
-    return ((ypred - ytrue) ** 2).sum(dim='time')
+    return ((ypred - ytrue) ** 2).sum(dim='time').astype('float32')
 
 
 def calc_fstat_pvalue(rss1, rss2, p1, p2, n, calc_p=False):
@@ -15,24 +18,21 @@ def calc_fstat_pvalue(rss1, rss2, p1, p2, n, calc_p=False):
     num = (rss1 - rss2) / (p2 - p1)
     denom = rss2 / (n - p2)
     f = num / denom
+    del num, denom
     p = None
     if calc_p:
         p = 1 - scipy.stats.f.cdf(f, p2 - p1, n - p2)
     return f, p
 
 
-def make_predictions_3D(
-    x, breakpoint, has_breakpoint, slope_total, slope1, slope2, int_total, int1, int2
-):
-    pred = (int_total + slope_total * x).transpose(*x.dims).astype('float32')
-
+def make_predictions_3D(x, pred, breakpoint, has_breakpoint, slope1, slope2, int1, int2):
     for i in range(1, len(x.time)):
         mask1 = has_breakpoint & (breakpoint == i) & (x < i)
-        pred1 = (int1 + slope1 * x).transpose(*x.dims).astype('float32')
+        pred1 = (int1 + slope1 * x).transpose(*pred.dims).astype('float32')
         pred = xr.where(mask1, x=pred1, y=pred)
 
         mask2 = has_breakpoint & (breakpoint == i) & (x >= i)
-        pred2 = (int2 + slope2 * x).transpose(*x.dims).astype('float32')
+        pred2 = (int2 + slope2 * x).transpose(*pred.dims).astype('float32')
         pred = xr.where(mask2, x=pred2, y=pred)
 
     return pred.astype('float32')
@@ -50,28 +50,30 @@ def linear_regression_3D(x, y, calc_p=True):
     """
     # 1. Compute data length, mean and standard deviation along time axis for further use:
     n = len(x.time)
-    ymean = y.mean(dim='time')
+    y = y.astype('float32')
+    ymean = y.mean(dim='time').astype('float32')
 
     if n >= 2:
         xmean = x.mean(dim='time')
-        xstd = x.std(dim='time')
 
         # 2. Compute covariance along time axis
-        cov = ((x - xmean) * (y - ymean)).sum(dim='time') / n
+        cov = (((x - xmean) * (y - ymean)).sum(dim='time') / n).astype('float32')
 
         # 5. Compute regression slope and intercept:
-        slope = cov / (xstd ** 2)
-        intercept = ymean - xmean * slope
+        slope = (cov / (x.std(dim='time') ** 2)).astype('float32')
+        del cov
+        intercept = (ymean - xmean * slope).astype('float32')
 
         # 6. Compute RSS
-        pred = ((slope * x) + intercept).transpose(*y.dims)
-        rss = calc_rss(y, pred)
+        pred = ((slope * x) + intercept).transpose(*y.dims).astype('float32')
+        rss = calc_rss(y, pred).astype('float32')
 
         # 7. Compute F-stat and p value
-        rss_null = calc_rss(y, ymean)
+        rss_null = calc_rss(y, ymean).astype('float32')
         fstat, pvalue = calc_fstat_pvalue(rss1=rss_null, rss2=rss, p1=1, p2=2, n=n, calc_p=calc_p)
 
-        del xmean, ymean, xstd, cov, pred, rss_null, fstat
+        # polyfit & curvefit in xarray
+        del xmean, ymean, pred, rss_null, fstat, y
 
     elif n == 1:
         zero_array = xr.DataArray(0, dims=ymean.dims, coords=ymean.coords)
@@ -81,11 +83,14 @@ def linear_regression_3D(x, y, calc_p=True):
         rss = zero_array
         pvalue = zero_array
 
+        del ymean
+
     return slope.astype('float32'), intercept.astype('float32'), rss.astype('float32'), pvalue
 
 
 def perform_change_detection(da):
     # 1. initialize parameter values
+    print(f'1. {datetime.now()}')
     # this assumes that we're performing chow test for a time series with no additional independent variables
     # thus degree of freedom (k) = 2 (intercept, slope)
     k = 2
@@ -98,14 +103,23 @@ def perform_change_detection(da):
     #     critical_value = 10.03  # 90% CI
 
     # 2. initialize x array as the independent variable (i.e. n timestep)
+    print(f'2. {datetime.now()}')
     # subtracting 1 to be consistent with python index
-    x = xr.DataArray(1, dims=da.dims, coords=da.coords).cumsum(dim='time').astype('int8') - int(1)
+    # TODO: have static numbers for
+    x = xr.DataArray(np.arange(n), dims=['time'], coords=[da.coords['time']])
+    da = da.astype('float32')
 
     # 3. fit one linear regression for entire time series
+    print(f'3. {datetime.now()}')
     slope_total, int_total, rss_total, p_total = linear_regression_3D(x=x, y=da, calc_p=True)
+    pred_total = (int_total + slope_total * x).transpose(*da.dims).astype('float32')
+    print(pred_total)
+    del slope_total, int_total, p_total
 
     # 4. for each break point, fit 2 linear regression model and assess the fit, save the best fit
+    print(f'4. {datetime.now()}')
     for i in range(1, n):
+        print(f'4.{i} {datetime.now()}')
         slope1, int1, rss1, _ = linear_regression_3D(
             x=x.isel(time=slice(None, i)), y=da.isel(time=slice(None, i)), calc_p=False
         )
@@ -117,6 +131,7 @@ def perform_change_detection(da):
         f_breakpoint, _ = calc_fstat_pvalue(
             rss1=rss_total, rss2=(rss1 + rss2), p1=k, p2=2 * k, n=n, calc_p=False
         )
+        del rss1, rss2
 
         # if the current f stat is larger than the current max f for a pixel, save the current values
         if i == 1:
@@ -130,50 +145,54 @@ def perform_change_detection(da):
         else:
             mask = f_breakpoint > max_f
             max_f = xr.where(mask, x=f_breakpoint, y=max_f)
+            del f_breakpoint
             breakpoint = xr.where(mask, x=i, y=breakpoint)
             output_slope1 = xr.where(mask, x=slope1, y=output_slope1)
             output_slope2 = xr.where(mask, x=slope2, y=output_slope2)
             output_int1 = xr.where(mask, x=int1, y=output_int1)
             output_int2 = xr.where(mask, x=int2, y=output_int2)
+            del mask
 
-        del slope1, slope2, int1, int2, rss1, rss2, f_breakpoint
+        del slope1, slope2, int1, int2
+
+    del rss_total
 
     # 5. If the best fit from break point regression is better than the critical f value, make predictions based on that model
+    print(f'5. {datetime.now()}')
     # else make prediction based on the 1 regression model
-    has_breakpoint = (max_f > critical_value).compute()
+    has_breakpoint = max_f > critical_value
+    del max_f
     pred = make_predictions_3D(
         x=x,
+        pred=pred_total,
         breakpoint=breakpoint,
         has_breakpoint=has_breakpoint,
-        slope_total=slope_total,
         slope1=output_slope1,
         slope2=output_slope2,
-        int_total=int_total,
         int1=output_int1,
         int2=output_int2,
-    )
+    ).transpose(*da.dims)
+    print(pred)
 
     del (
-        slope_total,
         output_slope1,
         output_slope2,
-        int_total,
         output_int1,
         output_int2,
-        rss_total,
-        max_f,
-        mask,
     )
 
     # 6. If we think there is a break point, get p value for the 2 piece, otherwise save the p value for 1 linear regression
+    print(f'6. {datetime.now()}')
     rss = calc_rss(da, pred)
     rss_null = calc_rss(da, da.mean(dim='time'))
-    _, p_breakpoint = calc_fstat_pvalue(rss1=rss_null, rss2=rss, p1=1, p2=4, n=n, calc_p=True)
+    _, p_total = calc_fstat_pvalue(rss1=rss_null, rss2=rss, p1=1, p2=k, n=n, calc_p=True)
+    _, p_breakpoint = calc_fstat_pvalue(rss1=rss_null, rss2=rss, p1=1, p2=2 * k, n=n, calc_p=True)
     pvalue = xr.where(has_breakpoint, x=p_breakpoint, y=p_total).compute()
     pvalue = pvalue.astype('float32')
     del rss, rss_null, p_breakpoint, p_total
 
     # 7. Update predictions based on p value
+    print(f'7. {datetime.now()}')
     pred = xr.where(pvalue <= 0.05, x=pred, y=da.mean(dim='time')).compute()
     pred = pred.astype('float32')
 
