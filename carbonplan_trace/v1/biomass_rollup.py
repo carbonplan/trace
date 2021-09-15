c9d2b6db5537bf4f7e4961bf117d37376ef64492
Joseph Hamman <jhamman@ucar.edu>
2021-09-15 15:17:48 -0700

    lint

--- carbonplan_trace/v1/biomass_rollup.py
+++ carbonplan_trace/v1/biomass_rollup.py
@@ -1,37 +1,34 @@
+import dask
+import fsspec
+import numcodecs
+import pandas as pd
+import xarray as xr
+
+from carbonplan_trace.metadata import get_cf_global_attrs
+from carbonplan_trace.tiles import tiles
 from carbonplan_trace.utils import zarr_is_complete
 from carbonplan_trace.v0.core import coarsen_emissions
-from carbonplan_trace.metadata import get_cf_global_attrs
-from carbonplan_trace.v0.data.load import _preprocess
 from carbonplan_trace.v0.data import cat
+from carbonplan_trace.v0.data.load import _preprocess
 from carbonplan_trace.v1 import utils
-import xarray as xr
-import pandas as pd
-import fsspec
-import dask
-from carbonplan_trace.tiles import tiles
-
-
-import numcodecs
 
 
 def open_biomass_tile(tile_id, version, y0=2014, y1=2021):
     input_tile_fn = f"s3://carbonplan-climatetrace/{version}/results/tiles/{tile_id}.zarr"
     ds = xr.open_zarr(input_tile_fn)
-    
-    # assign correct time coords 
+
+    # assign correct time coords
     time_coords = {'time': pd.date_range(f'{y0}-07-01', f'{y1}-07-01', freq='A')}
     ds = ds.assign_coords(time_coords)
-    
-    # use igbp land cover for masking 
+
+    # use igbp land cover for masking
     lat, lon = utils.get_lat_lon_tags_from_tile_path(tile_id)
     bounding_box = utils.parse_bounding_box_from_lat_lon_tags(lat, lon)
     igbp = utils.open_global_igbp_data(lat_lon_box=bounding_box)
     land_mask = (igbp.igbp > 0).any(dim='year')
-    ds['land_mask'] = utils.find_matching_records(
-        data=land_mask, lats=ds.lat, lons=ds.lon
-    )
+    ds['land_mask'] = utils.find_matching_records(data=land_mask, lats=ds.lat, lons=ds.lon)
     ds = ds.where(ds.land_mask)
-    
+
     return ds
 
 
@@ -56,26 +53,21 @@ def open_hansen_biomass_tile(tile_id, version):
     lat, lon = tile_id.split('_')
 
     # Hansen biomass
-    ds["agb"] = (
-        cat.gfw_biomass(lat=lat, lon=lon)
-        .to_dask()
-        .pipe(_preprocess)
-        .astype("float32")
-    )
+    ds["agb"] = cat.gfw_biomass(lat=lat, lon=lon).to_dask().pipe(_preprocess).astype("float32")
 
     return ds
 
 
 def coarsen_biomass_one_tile(
-    tile_id, 
-    get_biomass_ds_func, 
-    output_template, 
-    variables, 
-    version, 
-    skip_existing=True, 
+    tile_id,
+    get_biomass_ds_func,
+    output_template,
+    variables,
+    version,
+    skip_existing=True,
     coarsening_factor=100,
-    coarse_chunks={"lat": 400, "lon": 400}
-    ):
+    coarse_chunks={"lat": 400, "lon": 400},
+):
     """
     Given lat and lon to select a region, coarsen biomass by averaging
 
@@ -83,14 +75,14 @@ def coarsen_biomass_one_tile(
     ----------
     tile_id : str
         String specifying which 10x10-degree tile to process, e.g. `50N_120W`
-    get_biomass_ds_func : func 
-        A function that takes in two required params: tile_id and version, and returns the biomass ds to be coarsened 
+    get_biomass_ds_func : func
+        A function that takes in two required params: tile_id and version, and returns the biomass ds to be coarsened
     output_template : str
-        File name template for the output file, to be formatted with tile id 
-    variables : list 
-        Variables to be saved 
+        File name template for the output file, to be formatted with tile id
+    variables : list
+        Variables to be saved
     version : str
-        Version of biomass data to process 
+        Version of biomass data to process
 
     Returns
     -------
@@ -110,39 +102,43 @@ def coarsen_biomass_one_tile(
         ds = get_biomass_ds_func(tile_id=tile_id, version=version)
         print('coarsen')
         coarse_out = coarsen_emissions(
-            ds[variables], factor=coarsening_factor, mask_var=variables[0], method='mean').chunk(
-            coarse_chunks)
+            ds[variables], factor=coarsening_factor, mask_var=variables[0], method='mean'
+        ).chunk(coarse_chunks)
         coarse_out.attrs.update(get_cf_global_attrs())
 
         coarse_mapper.clear()
         encoding = {"compressor": numcodecs.Blosc()}
         print('writing')
         coarse_out[variables].to_zarr(
-            coarse_mapper, encoding={var: encoding for var in variables}, mode="w", consolidated=True)
+            coarse_mapper,
+            encoding={var: encoding for var in variables},
+            mode="w",
+            consolidated=True,
+        )
         return_status = 'coarsen-done'
 
     return (return_status,)
 
 
 def combine_all_tiles(
-    input_tile_template, 
-    output_global_fn, 
-    variables, 
+    input_tile_template,
+    output_global_fn,
+    variables,
     skip_existing=True,
-    coarse_chunks={"lat": 400, "lon": 400}
-    ):
+    coarse_chunks={"lat": 400, "lon": 400},
+):
     """
-    Combine all tiles specified with input tile template into a global output file 
+    Combine all tiles specified with input tile template into a global output file
 
     Parameters
     ----------
     input_tile_template : str
-        File name template for the input tile file, to be formatted with tile id 
+        File name template for the input tile file, to be formatted with tile id
     output_global_fn: str
-        File name of output file 
-    variables : list 
-        Variables to be saved 
-    """    
+        File name of output file
+    variables : list
+        Variables to be saved
+    """
     print('combining all tiles')
     mapper = fsspec.get_mapper(output_global_fn)
     checks = [f'{v}/0.0.0' for v in variables]
@@ -152,9 +148,7 @@ def combine_all_tiles(
         with dask.config.set(**{'array.slicing.split_large_chunks': False}):
             list_all_coarsened = []
             for tile_id in tiles:
-                sub = xr.open_zarr(
-                    input_tile_template.format(tile_id=tile_id), consolidated=True
-                )
+                sub = xr.open_zarr(input_tile_template.format(tile_id=tile_id), consolidated=True)
                 sub = sub.assign_coords(lat=sub.lat.round(6), lon=sub.lon.round(6))
                 list_all_coarsened.append(sub)
             ds = xr.combine_by_coords(
@@ -169,5 +163,5 @@ def combine_all_tiles(
 
         encoding = {"compressor": numcodecs.Blosc()}
         ds.to_zarr(mapper, encoding={var: encoding for var in variables}, consolidated=True)
-    
+
     return 'done'
